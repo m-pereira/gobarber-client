77d2ef5da9d5086a8d63e01295c16b0b02b37f4f
Mauricio Lima <mauricio@codebit.com.br>
2021-05-24 21:36:24 -0300

    chore: configure react-router-dom
    add history service

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import history from './services/history';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <h1>Go Barber</h1>
-    </>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function SignIn() {
+  return <h1>SignIn page</h1>;
+}
+
+export default SignIn;
