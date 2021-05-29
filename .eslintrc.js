3aaa79d1c3f4082b700a46a4092e55d0bbdc7be2
Mauricio Lima <mauricio@codebit.com.br>
2021-05-29 15:58:20 -0300

    feat: add custom component Route, as secure
    to redirect if user is not logged

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,6 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
