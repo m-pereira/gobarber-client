c0e9868b61d95a4a5164e8ae672fcfa8f9b0c13d
Mauricio Lima <mauricio@codebit.com.br>
2021-05-29 15:57:59 -0300

    chore: configure reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
+    'import/prefer-default-export': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
