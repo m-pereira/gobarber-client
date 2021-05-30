ade09e994eb21280fe15476067d0c5e857387f29
Mauricio Lima <mauricio@codebit.com.br>
2021-05-30 13:02:47 -0300

    feat: add unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
-const SignIn = () => (
-  <>
-    <img src={logo} alt='Gobarber-logo' />
+const SignIn = () => {
+  const handleSubmit = (data) => {
+    console.tron.log(data);
+  };
 
-    <form>
-      <input type='email' placeholder='Seu e-mail' />
-      <input type='password' placeholder='Sua senha secreta' />
+  return (
+    <>
+      <img src={logo} alt='Gobarber-logo' />
 
-      <button type='submit'>Acessar</button>
-      <Link to='/register'>Criar conta gratuita</Link>
-    </form>
-  </>
-);
+      <Form onSubmit={handleSubmit}>
+        <Input name='email' type='email' placeholder='Seu e-mail' />
+        <Input
+          name='password'
+          type='password'
+          placeholder='Sua senha secreta'
+        />
+
+        <button type='submit'>Acessar</button>
+        <Link to='/register'>Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
-const SignUp = () => (
-  <>
-    <img src={logo} alt='Gobarber-logo' />
+const SignUp = () => {
+  const handleSubmit = (data) => {
+    console.tron.log(data);
+  };
 
-    <form>
-      <input type='text' placeholder='Nome completo' />
-      <input type='email' placeholder='Seu e-mail' />
-      <input type='password' placeholder='Sua senha secreta' />
+  return (
+    <>
+      <img src={logo} alt='Gobarber-logo' />
 
-      <button type='submit'>Criar conta</button>
-      <Link to='/'>Já tenho uma conta</Link>
-    </form>
-  </>
-);
+      <Form onSubmit={handleSubmit}>
+        <Input name='name' type='text' placeholder='Nome completo' />
+        <Input name='email' type='email' placeholder='Seu e-mail' />
+        <Input
+          name='password'
+          type='password'
+          placeholder='Sua senha secreta'
+        />
+
+        <button type='submit'>Criar conta</button>
+        <Link to='/'>Já tenho uma conta</Link>
+      </Form>
+    </>
+  );
+};
 
 export default SignUp;
