9e0a543704e79c226c61c0a9b8d4d0609ef25a17
Mauricio Lima <mauricio@codebit.com.br>
2021-06-09 20:49:54 -0300

    feat: add Header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Content, Profile } from './styles';
+import logo from '~/assets/logo-header.svg';
+
+const Header = () => (
+  <Container>
+    <Content>
+      <nav>
+        <img src={logo} alt='GoBarber' />
+        <Link to='/dashboard'>DASHBOARD</Link>
+      </nav>
+
+      <aside>
+        <Profile>
+          <div>
+            <strong>Fulano de tal</strong>
+            <Link to='/profile'>Meu perfil</Link>
+          </div>
+
+          <img
+            src='https://images.apilist.fun/adorable_avatars_api.png'
+            alt='Avatar'
+          />
+        </Profile>
+      </aside>
+    </Content>
+  </Container>
+);
+
+export default Header;
--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from '~/Components/Header';
 import { Wrapper } from './styles';
 
-const DefaultLayout = ({ children }) => <Wrapper>{children}</Wrapper>;
+const DefaultLayout = ({ children }) => (
+  <Wrapper>
+    <Header>{children}</Header>
+  </Wrapper>
+);
 
 DefaultLayout.propTypes = {
   children: PropTypes.oneOfType([
