ebfe763585ead6f6135083fcd57b78fc5b4a24d9
Mauricio Lima <mauricio@codebit.com.br>
2021-06-10 20:14:09 -0300

    fix: fix font parameters

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,7 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 
 
   * {
@@ -23,11 +23,12 @@ export default createGlobalStyle`
   }
 
   body {
-    -webkit-font-smoothing: antialised;
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, button {
-    font: 14px, 'Roboto', sans-serif;
+    font-size: 14px;
+    font-family: 'Roboto', sans-serif;
   }
 
   a {
