1c431c6c25a8039de19edd57701c732353b7446e
Mauricio Lima <mauricio@codebit.com.br>
2021-06-20 12:01:58 -0300

    feat: add styles to Dashboard component

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { Container, Time } from './styles';
 
 const Dashboard = () => (
-  <>
-    <h1>Dashboard</h1>
-    <p>Alow galera do dashboard</p>
-  </>
+  <Container>
+    <header>
+      <button type='button'>
+        <MdChevronLeft size={36} color='#fff' />
+      </button>
+      <strong>31 de Maio</strong>
+      <button type='button'>
+        <MdChevronRight size={36} color='#fff' />
+      </button>
+    </header>
+
+    <ul>
+      <Time past>
+        <strong>08:00</strong>
+        <span>Mauricio de Lima</span>
+      </Time>
+      <Time available>
+        <strong>09:00</strong>
+        <span>Em aberto</span>
+      </Time>
+      <Time>
+        <strong>10:00</strong>
+        <span>Mauricio de Lima</span>
+      </Time>
+      <Time>
+        <strong>11:00</strong>
+        <span>Mauricio de Lima</span>
+      </Time>
+    </ul>
+  </Container>
 );
 
 export default Dashboard;
